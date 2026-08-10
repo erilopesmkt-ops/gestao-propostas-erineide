79c378edf1c1866ed8b470871304f521f8947740
erilopesmkt-ops <erilopesmkt@gmail.com>
2026-08-10 09:14:47 -0300

    Conecta CRM ao Supabase

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.CRM_CONFIG = {
   SUPABASE_URL: 'https://imorefnkropyantqqkho.supabase.co',
-  SUPABASE_PUBLISHABLE_KEY: 'SUA-CHAVE-PUBLISHABLE-AQUI'
+  SUPABASE_PUBLISHABLE_KEY: 'sb_publishable_NSo2cICrjLMqeQjbx7LCgw_M4bPVaD_'
 };
